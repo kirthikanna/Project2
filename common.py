e5458d51ff3cb2bb2682306eee145266136e105c
kirthikanna <150948986+kirthikanna@users.noreply.github.com>
2025-04-16 22:27:14 +0100

    Add files via upload

---
+++ common.py
@@ -0,0 +1,6 @@
+class Data:
+    URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    DASHBOARD_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+    TIMEOUT = 10
+    EXCEL_FILE = "C:\\Users\\keerthana\\PycharmProjects\\capstoneproject\\test_case1.xlsx"
+    SHEET_NUMBER = "Sheet1"
